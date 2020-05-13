d09b0b0c9c35b08c6a317bf3e8b63f99493369b9
Julian17072002 <55779857+Julian17072002@users.noreply.github.com>
2020-05-13 19:43:46 +0200

    kleine_Verbesserungen

--- src/CSVRead.java
+++ src/CSVRead.java
@@ -3,60 +3,43 @@ import java.io.FileReader;
 import java.util.Scanner;
 
 public class CSVRead {
- public static String [][] arr;
- public static int anzahlZeilen=12;
- public static int anzahlSpalten=33;
- public static String filelocation= "C:\\Users\\julia\\Desktop\\Privat\\workspace\\Projekt_Wahlanalyse\\Wahl_Daten.csv";
- public static String InputLine = "";
+	public static String [][] arr;
+ 	public static int anzahlZeilen = 12;
+ 	public static int anzahlSpalten = 33;
+ 	public static String filelocation= "C:\\Users\\julia\\Desktop\\Privat\\workspace\\Projekt_Wahlanalyse\\Wahl_Daten.csv";
+ 	public static String InputLine = "";
+ 	private static Scanner sc;
+
 	
 	static void einlesen() {
 		arr = new String [anzahlZeilen][anzahlSpalten];
-		Scanner sc= null;
-		int Rowc = 0;
+		sc = new Scanner(System.in);
+		int zeilen = 0;
 		
 		try {
-			sc= new Scanner (new BufferedReader(new FileReader(filelocation))); // file wird angelegt
+			sc = new Scanner (new BufferedReader(new FileReader(filelocation))); // file wird angelegt
 			
 			while (sc.hasNextLine()) {
-				InputLine= sc.nextLine();
+				InputLine = sc.nextLine();
 				String[] inArr = InputLine.split(";");
-				for (int x = 0; x < inArr.length; x++) {
-					arr[Rowc][x]=inArr[x];
+				for (int i = 0; i < inArr.length; i++) {
+					arr[zeilen][i] = inArr[i];
 				}
-				Rowc++;
+				zeilen++;
 			}
-		} catch (Exception e) {
-			System.out.println(e);
+		} 
+		catch (Exception e) {
+			System.out.println("!!!Error!!!");
 		}
-		
-		
-	}
-	public static void arrayAusgeben() {
-		for (int Rowc = 0; Rowc < anzahlZeilen; Rowc++) {
-			for (int Colc = 0; Colc < anzahlSpalten; Colc++) {
-				System.out.println(arr[Rowc][Colc]);
-			}
-			System.out.println();
-		}
-		return;
 	}
 	
-	static void getNumOfRowCol() {
-		Scanner sc= null;
-		System.out.println("Anzahl an Zeilen und Spalten werden hier berechnet");
-		try {
-			sc = new Scanner(new BufferedReader(new FileReader(filelocation)));
-			sc.useDelimiter(";");
-			while (sc.hasNextLine()) {
-				InputLine=sc.nextLine();
-				String[] inArr = InputLine.split(";");
-				anzahlZeilen++;
-				anzahlSpalten = inArr.length;
+	public static void arrayAusgeben() {
+		for (int zeilen = 0; zeilen < anzahlZeilen; zeilen++) {
+			for (int spalten = 0; spalten < anzahlSpalten; spalten++) {
+				System.out.println(arr[zeilen][spalten]);
 			}
-		} catch (Exception e ) {
-			System.out.println(e);
 		}
-		
-		
 	}
+	
+
 }
